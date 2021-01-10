5ed50a63942f3034f34e9bacd7a2418e4bcf63ea
cristian.ionas <cris.ionas@gmail.com>
2021-01-10 23:42:20 +0200

    Project init

---
+++ ElectronicsStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ElectronicsStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
