c5db560d62292294fc7a6196f744fbb90e65a76d
cristian.ionas <cris.ionas@gmail.com>
2021-01-11 16:38:16 +0200

    Modal Popup adapted for Login

---
+++ ElectronicsStore/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElectronicsStore.Models;
+
+namespace ElectronicsStore.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return PartialView("_LoginPartial");
+        }
+        public ActionResult CreateEdit(int? id)
+        {
+            UserDto model = new UserDto();
+            model.IsActive = true;
+            if (id.HasValue)
+            {
+                //Write your get user details code here.  
+            }
+            return PartialView("_CreateEdit", model);
+
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult CreateEdit(UserDto model)
+        {
+            //validate user  
+            if (!ModelState.IsValid)
+                return PartialView("_CreateEdit", model);
+
+
+            //save user into database   
+            return RedirectToAction("Index");
+        }
+    }
+}
--- ElectronicsStore/wwwroot/js/site.js
+++ ElectronicsStore/wwwroot/js/site.js
@@ -2,3 +2,65 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+(function ($) {
+    function Index() {
+        var $this = this;
+        function initialize() {
+
+            $(".popup").on('click', function (e) {
+                modelPopup(this);
+            });
+
+            function modelPopup(reff) {
+                var url = $(reff).data('url');
+
+                $.get(url).done(function (data) {
+                    debugger;
+                    $('#loginModal').find(".modal-dialog").html(data);
+                    $('#loginModal > .modal', data).modal("show");
+                });
+
+            }
+        }
+
+        $this.init = function () {
+            initialize();
+        };
+    }
+    $(function () {
+        var self = new Index();
+        self.init();
+    });
+}(jQuery));  
+
+
+(function ($) {
+    function Index() {
+        var $this = this;
+        function initialize() {
+
+            $(".popup").on('click', function (e) {
+                modelPopup(this);
+            });
+
+            function modelPopup(reff) {
+                var url = $(reff).data('url');
+
+                $.get(url).done(function (data) {
+                    debugger;
+                    $('#modal-create-edit-user').find(".modal-dialog").html(data);
+                    $('#modal-create-edit-user > .modal', data).modal("show");
+                });
+
+            }
+        }
+
+        $this.init = function () {
+            initialize();
+        };
+    }
+    $(function () {
+        var self = new Index();
+        self.init();
+    });
+}(jQuery));  
